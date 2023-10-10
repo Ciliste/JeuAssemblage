e180a6b20768ab50cd80d6b1e281ac8e98f942ea
Erreiip <lolpierre154@gmail.com>
2023-10-10 13:57:41 +0200

    Repair controller things

--- jeuAssemblage/src/main/Controller.java
+++ jeuAssemblage/src/main/Controller.java
@@ -2,22 +2,28 @@ package main;
 
 import java.util.Random;
 
+import model.PlayBoard;
+import view.MainFrame;
+
 public class Controller {
     
     private static Controller instance;
     public static Controller getInstance() {
 
         if (instance == null) {
-
             instance = new Controller();
+			instance.model = new PlayBoard(0, 0, 0);
+			instance.view = new MainFrame();
         }
 
         return instance;
     }
 
-	private Controller() {
+	private PlayBoard model;
+	private MainFrame view;
 
-	}
+
+	private Controller() {}
 
 	public int getSizeX(long seed) {
 
@@ -32,5 +38,13 @@ public class Controller {
 	public int getPiecesCount(long seed) {
 
 		return new Random(seed + 2).nextInt(10) + 5;
+	}
+
+	public boolean canBeAddedToBoard(Piece p) {
+		return this.model.canBeAddedToBoard(
+			p.getX(),
+			p.getY(),
+			p.getBounds()	
+		);
 	}
 }
