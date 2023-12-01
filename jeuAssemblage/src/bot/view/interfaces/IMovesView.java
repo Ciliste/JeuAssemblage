7a9d8e8bc1234a89cc68344f0497dab110089d27
erreiip <102669032+Erreiip@users.noreply.github.com>
2023-12-01 14:10:11 +0100

    [BOT] little refacto

--- jeuAssemblage/src/bot/view/interfaces/IMovesView.java
+++ jeuAssemblage/src/bot/view/interfaces/IMovesView.java
@@ -1,4 +1,5 @@
 package bot.view.interfaces;
 
-public interface IMovesView extends Runnable{
+public interface IMovesView extends Runnable {
+    //Only here if in the future we want to add some methods to the interface
 }
